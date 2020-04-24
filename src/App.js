bc2d3bb69d77c35aa75962d178ec0e04f167a8e2
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-04-24 14:42:01 -0300

    First ReactJS chalange finalized.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect( () => {
+    api.get('repositories').then(response => {
+      const repositories = response.data;
+      setRepositories([...repositories]);
+ 
+    });    
+    
+  }, [repositories])
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('/repositories', {
+      title:"ReactJS",
+	    url:"https://github.com/facebook/react",
+	    techs:["Java Script", "Node.js"]	
+    });
+    
   }
 
+
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
+         
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
